712f83b42ee64b7249fc38bf8d8d2a967e83d497
ivanbravo02 <a3522110075@alumno.uttehuacan.edu.mx>
2024-02-14 18:42:53 -0600

    first commit

---
+++ src/pages/MovieDetails.js
@@ -0,0 +1,6 @@
+import movie from "./movie.json";
+import styles from "./MovieDetails.module.css";
+
+export function MovieDetails(){
+    return <div className={styles.detailsContainer}>Details</div>
+}
